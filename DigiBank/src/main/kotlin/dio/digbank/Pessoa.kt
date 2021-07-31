02236f06d4bf0df149525364970b3890960f00a2
Jessica Beatriz <jessicabeatriz@id.uff.br>
2021-07-31 15:44:17 -0300

    adicionando inner class em pessoa

--- DigiBank/src/main/kotlin/dio/digbank/Pessoa.kt
+++ DigiBank/src/main/kotlin/dio/digbank/Pessoa.kt
@@ -4,8 +4,13 @@ class Pessoa {
     var nome:String = "jessica"
     var cpf:String = "111.111.111.22"
 
+    inner class Endereco{
+        var rua:String = "Rua dos Bobos"
+        var numero:Int = 0
+    }
+
     override fun toString(): String {
-        return "Nome: $nome\nCPF: $cpf"
+        return "Nome: $nome\nCPF: $cpf\nEndereco: ${Endereco().rua}, n°${Endereco().numero}"
     }
 }
 
