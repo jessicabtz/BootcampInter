a3fc1006557cf15f15cabde45dd5cdbac8655681
Jessica Beatriz <jessicabeatriz@id.uff.br>
2021-07-31 19:20:33 -0300

    atualiza saida

--- DigiBank/src/main/kotlin/dio/digbank/testes/AnalistaTeste.kt
+++ DigiBank/src/main/kotlin/dio/digbank/testes/AnalistaTeste.kt
@@ -4,5 +4,5 @@ import dio.digbank.Analista
 
 fun main(){
     val a = Analista("Rafaela", "555.555.555-55", 3700.00)
-    println(a.toString())
+    println(a)
 }
--- DigiBank/src/main/kotlin/dio/digbank/testes/ClienteTeste.kt
+++ DigiBank/src/main/kotlin/dio/digbank/testes/ClienteTeste.kt
@@ -5,5 +5,5 @@ import dio.digbank.clienteTipo
 
 fun main(){
     val c = Cliente("Jessica", "111.111.111.22", clienteTipo.PF)
-    println(c.toString())
+    println(c)
 }
