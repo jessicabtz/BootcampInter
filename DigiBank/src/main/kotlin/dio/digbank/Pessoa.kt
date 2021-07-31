604907af54125434841fd592aeaa1e04b16e04d4
Jessica Beatriz <jessicabeatriz@id.uff.br>
2021-07-31 16:26:32 -0300

    transformando pessoa em data class

--- DigiBank/src/main/kotlin/dio/digbank/Pessoa.kt
+++ DigiBank/src/main/kotlin/dio/digbank/Pessoa.kt
@@ -1,10 +1,6 @@
 package dio.digbank
 
-class Pessoa {
-    var nome:String = "jessica"
-
-    var cpf:String = "111.111.111.22"
-    private set
+data class Pessoa (val nome:String, val cpf:String) {
 
     override fun toString(): String {
         return "Nome: $nome\nCPF: $cpf"
---
+++ DigiBank/src/main/kotlin/dio/digbank/testes/PessoaTeste.kt
@@ -0,0 +1,8 @@
+package dio.digbank.testes
+
+import dio.digbank.Pessoa
+
+fun main(){
+    val p = Pessoa("Jessica", "111.111.111.22")
+    println(p.toString())
+}
