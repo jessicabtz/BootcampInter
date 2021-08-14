fc14c229d1cb432d6bc4c1545da28a117d145cee
Jessica Beatriz <jessicabeatriz@id.uff.br>
2021-08-14 11:58:33 -0300

    ajustando calculo imc e resultado

--- CalculadoraIMC/app/src/main/java/com/calculadora/calculadoraimc/MainActivity.kt
+++ CalculadoraIMC/app/src/main/java/com/calculadora/calculadoraimc/MainActivity.kt
@@ -11,23 +11,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val calcular = calcular
-        val altura = altura.text.toString()
-        val peso = peso.text.toString()
         var resultado = resultado
-        var str:String = ""
 
         calcular.setOnClickListener {
-            val imc = peso.toDouble()/pow((altura.toDouble()*0.01),2.0)
-            resultado.setText("Seu IMC eh: $imc!")
+            val peso = peso.text.toString()
+            val altura = altura.text.toString()
+            var str: String = ""
+
+            val imc:Double = peso.toDouble()/ pow((altura.toDouble()*0.01),2.0)
+            str = "Seu IMC e: ${String.format("%.2f",imc)}!"
             when{
-                imc <= 18.5 -> str = "IMC abaixo de 18.5 representa Abaixo do Peso"
-                imc < 25.0  -> str = "IMC abaixo de 18.5 representa Abaixo do Peso"
-                imc < 30    -> str = "IMC abaixo de 18.5 representa Abaixo do Peso"
-                imc < 35    -> str = "IMC abaixo de 18.5 representa Abaixo do Peso"
-                imc < 40    -> str = "IMC abaixo de 18.5 representa Abaixo do Peso"
-                else -> str = "IMC abaixo de 18.5 representa Abaixo do Peso"
-            }
-          resultado.setText("$resultado\n$str")
+               imc <= 18.5 -> str += "\nIMC abaixo de 18.5 representa Abaixo do Peso"
+               imc < 25.0  -> str += "\nIMC maior que 18.5 e menor que 25 representa Peso Normal"
+               imc < 30    -> str += "\nIMC maior ou igual a 25 e menor que 30 representa Sobrepeso"
+               imc < 35    -> str += "\nIMC maior ou igual a 30 e menor que 35 representa Obesidade grau 1"
+               imc < 40    -> str += "\nIMC maior ou igual a 35 e menor que 40 representa Obesidade grau 2"
+               else        -> str += "\nIMC maior ou igual a 40 representa Obesidade Grau 3 ou Morbida"
+           }
+           resultado.setText("$str")
         }
     }
+
+}
+fun main() {
+    println("$")
 }
