c2a49e523ea9e6dd8c0cc1a435ae785af5f2321c
Jessica Beatriz <jessicabeatriz@id.uff.br>
2021-08-28 11:34:43 -0300

    inserindo um menu no layout

--- LIstadeContatos/app/src/main/java/br/com/jbtz/listadecontatos/MainActivity.kt
+++ LIstadeContatos/app/src/main/java/br/com/jbtz/listadecontatos/MainActivity.kt
@@ -2,6 +2,9 @@ package br.com.jbtz.listadecontatos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
+import android.view.*
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -30,5 +33,29 @@ class MainActivity : AppCompatActivity() {
                 Contact("Igor Ferrani", "(11)11111-1111", "img2.png")
             )
         )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    private fun showToast(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.item1 ->{
+                showToast("Exibindo item do Menu 1")
+                return true
+            }
+            R.id.item2 -> {
+                showToast("Exibindo item do Menu 2")
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
