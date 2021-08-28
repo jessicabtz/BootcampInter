5f4f594b3bd01fe75f4b4278e4e6e91624592978
Jessica Beatriz <jessicabeatriz@id.uff.br>
2021-08-28 10:52:36 -0300

    implementando métodos da main activity

--- LIstadeContatos/app/src/main/java/br/com/jbtz/listadecontatos/MainActivity.kt
+++ LIstadeContatos/app/src/main/java/br/com/jbtz/listadecontatos/MainActivity.kt
@@ -2,10 +2,33 @@ package br.com.jbtz.listadecontatos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val rvList: RecyclerView by lazy { findViewById<RecyclerView>(R.id.rv_list) }
+    private val adapter = ContactAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindView()
+        updateList()
+    }
+
+    private fun bindView(){
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this
+        )
+    }
+
+    private fun updateList(){
+        adapter.updateList(
+            arrayListOf(
+                Contact("Jéssica Beatriz","(00)00000-0000","img.png"),
+                Contact("Igor Ferrani", "(11)11111-1111", "img2.png")
+            )
+        )
     }
 }
