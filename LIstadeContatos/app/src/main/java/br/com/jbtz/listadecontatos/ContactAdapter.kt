88d908463cb25e9569c7cf087f2d085fc308dfb7
Jessica Beatriz <jessicabeatriz@id.uff.br>
2021-08-28 10:18:22 -0300

    implementando métodos da adapter

--- LIstadeContatos/app/src/main/java/br/com/jbtz/listadecontatos/ContactAdapter.kt
+++ LIstadeContatos/app/src/main/java/br/com/jbtz/listadecontatos/ContactAdapter.kt
@@ -1,25 +1,29 @@
 package br.com.jbtz.listadecontatos
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class ContactAdapter : RecyclerView.Adapter<>(){// fazer o gerenciamento da lista como um td
+class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>(){// fazer o gerenciamento da lista como um td
     private val list:MutableList<Contact> = mutableListOf()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ??? {
-    TODO("Not yet implemented")
-}
-
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item, parent, false)
+        return ContactAdapterViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ???, position: Int) {
-        TODO("Not yet implemented")
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
+        holder.bind(list[position])
     }
 
     class ContactAdapterViewHolder(itemView: View) :RecyclerView.ViewHolder(itemView){ // gerencia cada elemento da lita
+        fun bind(contact: Contact){
 
+        }
     }
 }
